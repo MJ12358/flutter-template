d7f58c5b6856a2116ee00e3e0feac2014ae42b9c
MJ12358 <wtpltlf.dev@gmail.com>
2026-05-29 20:54:36 -0500

    add remote database

---
+++ lib/data/datasources/firebase/firebase_database.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter_template/core/constants.dart';
+import 'package:flutter_template/domain/core/remote_database.dart';
+
+class FirebaseDatabase implements RemoteDatabase<Object> {
+  FirebaseCrashlytics get _crashlytics => FirebaseCrashlytics.instance;
+
+  @override
+  Future<FirebaseDatabase> init() async {
+    // await Firebase.initializeApp(
+    //   options: DefaultFirebaseOptions.currentPlatform,
+    // );
+
+    if (Build.isDebug) {
+      await _crashlytics.setCrashlyticsCollectionEnabled(false);
+    }
+
+    return this;
+  }
+
+  @override
+  Object get db {
+    throw UnimplementedError();
+  }
+}
